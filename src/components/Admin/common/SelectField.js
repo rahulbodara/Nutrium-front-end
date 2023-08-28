ae825ad694fc2425e0ff5cd9b349904f92b7d619
rahulbodara <rb.globalia@gmail.com>
2023-08-28 12:21:44 +0530

    fixed the dropdown issue

--- src/components/Admin/common/SelectField.js
+++ src/components/Admin/common/SelectField.js
@@ -1,46 +1,57 @@
-import React, { useEffect, useRef } from 'react'
-const SelectField = (props) => {
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        if (name.includes(".")) {
-          const [parentName, childName] = name.split(".");
-          props.setFormData((prevFormData) => ({
-            ...prevFormData,
-            [parentName]: {
-              ...prevFormData[parentName],
-              [childName]: value,
-            },
-          }));
-        } else {
-          props.setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: value,
-          }));
-        }
-      };
-    return (
-        <div className={`flex select-none ${props?.className}`}>
-            <div className='field-title min-w-[160px] flex-basis-[160px] flex border bg-[#FAFAFB] text-[1.1em] items-center z-[1] px-2.5 py-[5px] border-solid border-[#EEEEEE]'>
-                <label className='inline-block max-w-full'>{props?.label}</label>
-            </div>
-            <div className='flex flex-grow'>
-                <div className='relative flex-grow select-none border border-[#EEEEEE]'>
-                    <select
-                        className="w-full p-[10px] text-[#6e7c91] border-none focus:outline-none focus:ring-0 "
-                        name={props.name}
-                        value={props.value ? props.value: ''}
-                        onChange={handleChange}
-                    > 
-                        {props?.option?.map((item, index) => (
-                            <>
-                                <option key={index} value={item.value}>{item.name}</option>
-                            </>
-                        ))}
-                    </select>
-                </div>
-            </div>
-        </div>
-    )
-}
+import React, { useEffect, useRef } from "react";
+import "select2/dist/css/select2.min.css";
+import $ from "jquery";
+import "select2";
 
-export default SelectField
+const SelectField = (props) => {
+  const selectRef = useRef(null);
+
+  useEffect(() => {
+    const $select = $(selectRef.current);
+
+    // Initialize select2
+    $select.select2({
+      minimumResultsForSearch: props.searchOption === false ? Infinity : 0,
+    });
+
+    // Handle change event
+    $select.on("change", (e) => {
+      const selectedValue = e.target.value;
+      if (props?.onChange) {
+        props?.onChange(selectedValue);
+      }
+    });
+
+    return () => {
+      $select.off("change");
+      $select.select2("destroy");
+    };
+  }, [props?.searchOption]);
+
+  return (
+    <div className={`flex select-none ${props.className}`}>
+      <div
+        className={`${props.LabelclassName} field-title min-w-[160px] flex-basis-[160px] flex border bg-[#FAFAFB] text-[1.1em] items-center z-[1] px-2.5 py-[5px] border-solid border-[#EEEEEE]`}
+      >
+        <label className="inline-block max-w-full">{props.label}</label>
+      </div>
+      <div className="flex flex-grow">
+        <div className="relative flex-grow select-none border border-[#EEEEEE]">
+          <select
+            ref={selectRef}
+            name={props.name}
+            className="w-full mt-2 p-[10px] text-[#6e7c91] rounded-md"
+          >
+            {props.option?.map((item, index) => (
+              <option key={index} value={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectField;
