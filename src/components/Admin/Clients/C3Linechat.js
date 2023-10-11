82cbb54f99b1f9a783b88eb2911762289a9408cd
rahulbodara <rb.globalia@gmail.com>
2023-10-11 18:14:28 +0530

    created deliverables page

--- src/components/Admin/Clients/C3Linechat.js
+++ src/components/Admin/Clients/C3Linechat.js
@@ -1,36 +1,54 @@
-
-
-
 import React, { useEffect } from 'react';
 import c3 from 'c3';
 import 'c3/c3.css';
 
-function DonutChart({ data, chartId }) {
+function LineChart({ data, chartId }) {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             var chart = c3.generate({
-                bindto: `#${chartId}`, // The HTML element to bind the chart to
+                bindto: `#${chartId}`,
 
                 data: {
+                    x: 'My Custom Label',
                     columns: [
-                        ['data1', 30, 200, 100, 400, 150, 250, 50, 100, 250]
+                        ['My Custom Label', '2023-09-04', '2023-09-11', '2023-09-18', '2023-09-25', '2023-10-02'],
+                        ['Meal plan', 0, 0, 0, 0, 0, 0],
+                        ['Food diary', 0, 0, 0, 0, 0, 0],
+                        ['Appointment', 0, 0, 0, 0, 0, 0],
+                        ['Follow up', 0, 0, 0, 0, 0, 0],
+                        ['Question', 0, 0, 0, 0, 0, 0],
+                        ['birthday', 0, 0, 0, 0, 0, 0],
                     ]
                 },
                 axis: {
                     x: {
-                        type: 'category',
-                        categories: ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6', 'cat7', 'cat8', 'cat9']
+                        type: 'timeseries',
+                        tick: {
+                            format: '%b %d' // Format to show only month and day
+                        },
+                    },
+                    y: {
+                        show: false
+                    },
+                },
+                tooltip: {
+                    format: {
+                        title: function (x) {
+                            const date = new Date(x);
+                            const month = date.toLocaleString('default', { month: 'long' });
+                            const day = date.getDate();
+                            return 'Week of ' + month + ' ' + day;
+                        },
                     }
                 }
             });
-
             return () => {
                 chart.destroy();
             };
         }
-    }, []);
+    }, [chartId]);
 
-    return <div id={chartId} className='w-[450px] h-[150px]' ></div>;
+    return <div id={chartId} className='w-[450px] h-[200px]'></div>;
 }
 
-export default DonutChart;
+export default LineChart;
